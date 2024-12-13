55ee6f5fc729c0e68b36d5b728152aa7f74dbfc8
Guy-Teubbe <antoine.bolzon@labom2iformation.fr>
2024-12-13 12:15:56 +0100

    Antoine - Recalcul inventaire après utilisation

--- clotoine_main_exe.py
+++ clotoine_main_exe.py
@@ -1,4 +1,5 @@
 import random
+import copy
 #FICHIER PRINCIPAL D'EXECUTION
 #
 # FILTHY WANKERZ III
@@ -13,19 +14,20 @@ import random
 
 
 
-Objet = {"1":{"nom":"Potion de Santé","propriété":"Vie","action":"+","min":1,"max":50},
-         "2":{"nom":"Poison de Vie","propriété":"Vie","action":"-","min":0,"max":15},
-         "3":{"nom":"Potion de Defense","propriété":"Defense","action":"+","min":2,"max":2},
-         "4":{"nom":"Poison de Faiblesse","propriété":"Defense","action":"-","min":0,"max":2}}
+Objet = {"1":{"Nom":"Potion de Santé","propriété":"Vie","action":"+","min":1,"max":50},
+         "2":{"Nom":"Poison de Vie","propriété":"Vie","action":"-","min":0,"max":15},
+         "3":{"Nom":"Potion de Defense","propriété":"Defense","action":"+","min":2,"max":2},
+         "4":{"Nom":"Poison de Faiblesse","propriété":"Defense","action":"-","min":0,"max":2}}
 
 
 Inventaire1 = {"1":Objet["1"],"2":Objet["1"],"3":Objet["1"]}
 Inventaire2 = {"1":Objet["1"],"2":Objet["1"],"3":Objet["2"]}
 
-Classe = {"Nécromancien":{"Vie":"+2"},"Zoomancien":{"Defense":"+1"},"Copromancien":{"Objet":Objet["2"]}}
+Classe = {"Nécromancien":{"Nom":"Nécromancien","Vie":"+2"},"Zoomancien":{"Nom":"Zoomancien","Defense":"+1"},"Copromancien":{"Nom":"Zoomancien","Objet":Objet["2"]}}
 
-Personnage1 = {"Nom":"Bandmou","Classe":Classe["Nécromancien"],"Niveau":1,"Vie":12,"Inventaire":Inventaire1,"Defense":9,"XP":0}
-Personnage1 = {"Nom":"Moudujon","Classe":Classe["Copromancien"],"Niveau":1,"Vie":10,"Inventaire":Inventaire2,"Defense":10,"XP":0}
+Personnage1 = {"Nom":"Bandmou","Classe":Classe["Nécromancien"]["Nom"],"Niveau":1,"Vie":12,"Inventaire":Inventaire1,"Defense":9,"XP":0}
+Personnage2 = {"Nom":"Moudujon","Classe":Classe["Copromancien"]["Nom"],"Niveau":1,"Vie":10,"Inventaire":Inventaire2,"Defense":10,"XP":0}
+
 
 #Création des  classes
 
@@ -38,22 +40,17 @@ def monter_niveau(perso = {}):
     perso["Niveau"] += 1
     perso["Vie"] += 20
 
-monter_niveau(Personnage1)
-print(Personnage1)
+
 
 #attaquer
-
-
-<<<<<<< HEAD
-def ajout_invent(var = {}):
-=======
+def afficherPerso(perso):
+     print(f"{perso["Nom"]}\n-Classe: {perso["Classe"]}\n-Niveau: {perso["Niveau"]}\n-Vie: {perso["Vie"]}\n-Defense: {perso["Defense"]}\n-XP: {perso["XP"]}\n\nINVENTAIRE\n{perso["Inventaire"]}")
 
 
 ########################
 #ajouter à l'inventaire#
 ########################
-def ajout_invent(var):
->>>>>>> cd149a311801d3f429561b337fc793978e56cc01
+def ajout_invent(var = {}):
     print(Objet)
     nb = input("Numéro de l'objet : ")
 
@@ -65,23 +62,64 @@ def ajout_invent(var):
 # print(Inventaire1)
 
 
+#############################
+#Recalculer Index Inventaire#
+#############################
+def recalculerIndexInventaire(inventaire,cle):
+    print(f"Inventaire avant recalcul:\n{inventaire}")
+
+
+    #for i in range(1,len(inventaire)+1):
+
+
+    # for key in range(1,len(inventaire)+1):
+    #     if int(key) > int(cle):
+    #         itemDecale = copy.copy(inventaire[key])
+    #         inventaire[str(int(key)-1)] = itemDecale
+
+    #         del inventaire[key]
+
+    print(f"Inventaire après recalcul:\n{inventaire}")
+            
+
 
 ########################
 #utilisation d'un objet#
 ########################
-def utiliserObjet(objet,cible):
+def utiliserObjet(objet,cible,inventaire,cle):
     #ajouter les bénéfices de l'objet sur le personnage
         prop = objet["propriété"]
         action = objet["action"]
         val = random.randint(objet["min"],objet["max"])
 
+        
         if action == "+":
+            print(f"{objet["Nom"]} va ajouter {val} pts de {prop} à {cible["Nom"]}")
             cible[prop] += val
         else:
+            print(f"{objet["Nom"]} va retirer {val} pts de {prop} à {cible["Nom"]}")
             cible[prop] -= val
 
+    #retirer de l'inventaire
+        del inventaire[cle]
+
+        recalculerIndexInventaire(inventaire,cle)
     
         
 
 
 
+def executionPrg():
+     
+    monter_niveau(Personnage1)
+    afficherPerso(Personnage1)
+    input(">")
+    utiliserObjet(Objet["1"],Personnage1,Inventaire1,"1")
+    input(">")
+
+    afficherPerso(Personnage1)
+    input(">")
+    recalculerIndexInventaire(Inventaire1)
+
+
+executionPrg()
